9bd6bf3f933332d6b7a5d071989cc1b9af5658a0
Henry Le <hlsoftdev@proton.me>
2025-04-20 10:35:05 -0600

    Remove react-router-bootstrap dependency and update MovieHeader component to use Link from react-router-dom for navigation

--- src/components/movieheader.js
+++ src/components/movieheader.js
@@ -1,66 +1,79 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { LinkContainer } from 'react-router-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutUser } from "../actions/authActions";
 import { BsFilm, BsSearch, BsInfoCircle, BsPerson } from 'react-icons/bs';
+import { Link } from 'react-router-dom'; // Import Link directly
 
 const MovieHeader = () => {
     const dispatch = useDispatch();
     const loggedIn = useSelector(state => state.auth.loggedIn);
     const username = useSelector(state => state.auth.username);
     const selectedMovie = useSelector(state => state.movie.selectedMovie);
-    
+
     const handleLogout = () => {
         dispatch(logoutUser());
     };
-    
+
     return (
         <Navbar expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand>
-                    <BsFilm className="me-2" /> Movie Review App
-                </Navbar.Brand>
+                {/* ... Navbar.Brand ... */}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <LinkContainer to="/movielist">
-                            <Nav.Link disabled={!loggedIn}>
-                                <BsFilm className="me-1" /> Movies
-                            </Nav.Link>
-                        </LinkContainer>
-                        
-                        <LinkContainer to="/search">
-                            <Nav.Link disabled={!loggedIn}>
-                                <BsSearch className="me-1" /> Search
-                            </Nav.Link>
-                        </LinkContainer>
-                        
+                        {/* Use Nav.Link with 'as={Link}' */}
+                        <Nav.Link
+                            as={Link}
+                            to="/movielist"
+                            disabled={!loggedIn}
+                            className="nav-link" // Add Bootstrap class if needed
+                             style={loggedIn ? {} : { pointerEvents: 'none', color: 'rgba(255, 255, 255, 0.55)' }} // Manual disable styling
+                        >
+                            <BsFilm className="me-1" /> Movies
+                        </Nav.Link>
+
+                        <Nav.Link
+                            as={Link}
+                            to="/search"
+                            disabled={!loggedIn}
+                            className="nav-link"
+                             style={loggedIn ? {} : { pointerEvents: 'none', color: 'rgba(255, 255, 255, 0.55)' }}
+                        >
+                            <BsSearch className="me-1" /> Search
+                        </Nav.Link>
+
                         {selectedMovie && (
-                            <LinkContainer to={'/movie/' + selectedMovie._id}>
-                                <Nav.Link disabled={!loggedIn}>
-                                    <BsInfoCircle className="me-1" /> Movie Detail
-                                </Nav.Link>
-                            </LinkContainer>
+                             <Nav.Link
+                                as={Link}
+                                to={'/movie/' + selectedMovie._id}
+                                disabled={!loggedIn}
+                                className="nav-link"
+                                 style={loggedIn ? {} : { pointerEvents: 'none', color: 'rgba(255, 255, 255, 0.55)' }}
+                            >
+                                <BsInfoCircle className="me-1" /> Movie Detail
+                            </Nav.Link>
                         )}
                     </Nav>
                     <Nav>
-                        <LinkContainer to="/signin">
-                            <Nav.Link>
-                                {loggedIn ? (
-                                    <span>
-                                        <BsPerson className="me-1" /> {username} {' '}
-                                        <button className="btn btn-sm btn-outline-light ms-2" onClick={handleLogout}>
-                                            Logout
-                                        </button>
-                                    </span>
-                                ) : (
-                                    <span>
-                                        <BsPerson className="me-1" /> Login / Register
-                                    </span>
-                                )}
+                         {/* Login/Logout link needs careful handling */}
+                         {/* If logged out, it's a link to /signin */}
+                         {!loggedIn && (
+                             <Nav.Link as={Link} to="/signin" className="nav-link">
+                                 <BsPerson className="me-1" /> Login / Register
+                             </Nav.Link>
+                         )}
+                         {/* If logged in, it's not really a link, just displays info and a button */}
+                         {loggedIn && (
+                            <Nav.Link as="span"> {/* Use span or div if not a link */}
+                                <span>
+                                    <BsPerson className="me-1" /> {username} {' '}
+                                    <button className="btn btn-sm btn-outline-light ms-2" onClick={handleLogout}>
+                                        Logout
+                                    </button>
+                                </span>
                             </Nav.Link>
-                        </LinkContainer>
+                         )}
                     </Nav>
                 </Navbar.Collapse>
             </Container>
